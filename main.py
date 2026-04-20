d05f38a582fbc7578ddd0a94dee3793b70925f46
Cam0203 <camicastillo0203@outlook.es>
2026-04-19 19:30:46 -0500

    corrijo cors duplicado

--- main.py
+++ main.py
@@ -30,7 +30,6 @@ app.add_middleware(
         "http://127.0.0.1:5500",
         "http://localhost:5500"
     ],
-    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
